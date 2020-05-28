4228c1de9f7bd7d60aecd0fc82d94bbf3186ba79
granzon <1978882990@qq.com>
2020-05-29 00:30:11 +0800

    fix: 删除多余元素

--- js/slide.js
+++ js/slide.js
@@ -14,7 +14,6 @@ $(document).ready(function () {
         let li = document.createElement('li');
         $('#paginationWrap ul').append(li);
     });
-    countSlides();
     pagination();
     $('#sliderWrap').hover(
             function () {
@@ -34,7 +33,6 @@ function slideLeft() {
         pos = totalSlides - 1;
     }
     $('#sliderWrap ul#slider').css('left', -(sliderWidth * pos));
-    countSlides();
     pagination();
 }
 
@@ -44,12 +42,7 @@ function slideRight() {
         pos = 0;
     }
     $('#sliderWrap ul#slider').css('left', -(sliderWidth * pos));
-    countSlides();
     pagination();
-}
-
-function countSlides() {
-    $('#counter').html(pos + 1 + ' / ' + totalSlides);
 }
 
 function pagination() {
